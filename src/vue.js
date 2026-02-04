114313678facf37456441d45e6a55a772497b8e7
Marc Vilella <mvilella.and@gmail.com>
2026-02-04 23:46:57 +0100

    refactor: move config files to src/ directory
    - Move index.js to src/index.js
    - Move vue.js to src/vue.js
    - Rewrite configs to ESLint 9 flat config format
    - Use ES module import/export syntax
    - Replace airbnb-base with manually configured rules
    - Update to typescript-eslint unified package
    - Switch to eslint-plugin-import-x for flat config support
    - Configure eslint-import-resolver-typescript

---
+++ src/vue.js
@@ -0,0 +1,56 @@
+import vuePlugin from 'eslint-plugin-vue';
+import { createTypeScriptImportResolver } from 'eslint-import-resolver-typescript';
+import baseConfig from './index.js';
+
+export default [
+  // Spread base config
+  ...baseConfig,
+
+  // Vue plugin recommended config for Vue 3
+  ...vuePlugin.configs['flat/recommended'],
+
+  // Override parser for Vue files with TypeScript
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.vue'],
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+  },
+
+  // Custom Vue rules
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue/singleline-html-element-content-newline': 'off',
+      'vue/component-tags-order': ['error', {
+        order: ['template', 'script', 'style'],
+      }],
+      'vue/html-closing-bracket-newline': ['error', {
+        singleline: 'never',
+        multiline: 'never',
+      }],
+      'vue/max-attributes-per-line': ['error', {
+        singleline: 4,
+        multiline: 1,
+      }],
+    },
+  },
+
+  // Update import resolver settings for .vue files
+  {
+    files: ['**/*.vue'],
+    settings: {
+      'import-x/resolver-next': [
+        createTypeScriptImportResolver({
+          alwaysTryTypes: true,
+          extensions: ['.ts', '.vue', '.json', '.yaml'],
+        }),
+      ],
+    },
+  },
+];
